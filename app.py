0962a5bebcdb7e380b757d42a49627c282e27160
user <kr447707@dal.ca>
2022-02-28 18:51:30 -0400

    UserAuthentication API for Administrator and Resident User

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import boto3
-from flask import Flask, request, json
+from flask import Flask, request, json,jsonify
 from boto3.dynamodb.conditions import Key
+from flask_cors import CORS
 
+client = boto3.client('cognito-idp')
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('userInfo')
 
-app = Flask(__name__)
+app=Flask(__name__)
+CORS(app)
 if app.config("ENV") == "production":
     app.config.from_object("configs.dev_config.ProductionConfig")
 else:
@@ -13,18 +16,39 @@ else:
     # retrieve anywhere in the project using app.config("key")
 
 
-@app.route("/user/authentication", methods=['POST', 'GET'])
+@app.route("/user/authentication",methods=['POST','GET'])
 def index():
-    json_input = json.loads(json.dumps(request.json))
-    response = table.scan(
-        FilterExpression=Key('user_Name').eq(json_input['userName']) & Key('user_Password').eq(json_input['password'])
-    )
-
-    if len(response['Items']) == 1:
-        return "Authenticate successfully"
+    jsonInput = request.get_json()
+    userName=jsonInput['userName']
+    password=jsonInput['password']
+    adminResult=checkAdminUser(userName,password)
+    if adminResult!="Admin":
+        response = table.scan(
+            FilterExpression=Key('user_Name').eq(userName) & Key('user_Password').eq(password)
+        )
+        if len(response['Items'])==1:
+            return jsonify(success=True, data="Resident Authenticate successfully")
+        else:
+            return jsonify(success=False, data="Invalid UserName and Password")
     else:
-        return "Invalid UserName and Password"
+        return jsonify(success=True, data="Administrator Authenticate successfully")
 
+def checkAdminUser(username, password):
+    try:
+      CognitoResponse = client.initiate_auth(
+            ClientId="2g2bbfsshtrt34fsp15sacir1r",
+            AuthFlow='USER_PASSWORD_AUTH',
+            AuthParameters={
+                'USERNAME':username,
+                'PASSWORD':password,
+            })
+    except client.exceptions.NotAuthorizedException:
+        return None
+    except client.exceptions.UserNotConfirmedException:
+        return None
+    except Exception as e:
+        return None
+    return "Admin"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
